af1f7f6cb5ffd41aa742c5d1cce3a979aaaf0e43
jezmin <jezmin@users.noreply.github.com>
2019-06-29 09:00:20 -0700

    Add comments to UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -1,3 +1,22 @@
+/******************************************************************************
+ *  Compilation:  javac UserInput.java
+ *  Execution:    java UserInput
+ *
+ *  Prompts user for a username and prints out Username is <username>.
+ *
+ *  % java UserInput
+ *  Enter username
+ *  Jezmin
+ *  Username is: Jezmin
+ *
+ *  These 19 lines of text are comments. They are not part of the program;
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
 import java.util.Scanner; // import the Scanner class 
 
 class UserInput {
